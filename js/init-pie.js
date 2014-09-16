a978da6c8236dc2f924da87e4e6412372ce5f3e6
gurieveugen <seejay@MacBook-Pro-SeeJay.local>
2014-09-16 16:17:58 +0300

    1.03

--- js/init-pie.js
+++ js/init-pie.js
@@ -1,6 +1,7 @@
-$(function() {
-	
-    $('.multi-twitter .text-holder, blockquote .message , blockquote .message q, , .tweet-box .message, .coming-box .btn-post, .coming-box .row input[type="text"],#main #tribe-events-content, #content, .form-touch input[type="text"],.form-touch textarea, .form-touch .btn-post, .links-holder, .links-holder h3, #slider li img').each(function() {
+jQuery(function() {	
+	// jQuery('.multi-twitter .text-holder').each(function(){ PIE.attach(this) });
+
+    jQuery('.multi-twitter .text-holder, blockquote .message, blockquote .message q, .tweet-box .message, .coming-box .btn-post, .coming-box .row input[type="text"], #main #tribe-events-content, #content, .form-touch input[type="text"], .form-touch textarea, .form-touch .btn-post, .links-holder, .links-holder h3, #slider li img').each(function() {
         PIE.attach(this);
     });
 });
